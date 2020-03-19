fa98aa09306567438d7b2b80462e20adb90f3fb2
SamuelAxelsson <samuel@vatterliden.se>
2020-03-19 20:56:46 +0100

    nu har vi två streck som är random utplacerade

--- Bilbana/Program.cs
+++ Bilbana/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bilbana
@@ -10,7 +11,29 @@ namespace Bilbana
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int bilposition = 40;
+            int counter = 0;
+            int vänsterkant = 30;
+            int bredd = 20;
+            Random random = new Random();
+            while (true)
+            {
+                string väg = "".PadRight(vänsterkant, ' ')+   "#".PadRight(bredd, ' ') + "#".PadRight(80 - vänsterkant - bredd - 3);
+                väg = väg.Substring(0, bilposition) + "o" + väg.Substring (bilposition - 1);
+                Console.WriteLine(väg);
+                counter = counter + 1;
+                vänsterkant = vänsterkant + random.Next(-2, 3);
+                if (vänsterkant < 0)
+                {
+                    vänsterkant = 0;
+                }
+                if (vänsterkant + bredd + 2 > 80)
+                {
+                    vänsterkant = 80 - bredd - 2;
+                }
+                Thread.Sleep(100);
+            }
+            
         }
     }
 }
